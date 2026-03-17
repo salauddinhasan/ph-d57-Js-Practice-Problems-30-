89848353fc303659d379ef3b75fa5e51e7f51331
salauddin <salauddin@hasan.com>
2026-03-17 13:25:07 +0600

    Solved basic JS problems: even/odd, positive/negative, largest number, loop table

---
+++ app.js
@@ -0,0 +1,108 @@
+// 1️⃣ Even or Odd
+
+// একটা number input নাও।
+// বলতে হবে number even নাকি odd।
+
+// let num = 7;
+
+// if(num % 2 === 0) {
+//     console.log('Event');
+// }else{ 
+//     console.log('Odd')
+// }
+
+
+// 2️⃣ Positive / Negative
+
+// একটা number দিলে বলবে:
+
+// positive
+
+// negative
+// zero
+
+
+// let num = 0;
+// if(num >= 1) {
+//     console.log('Positive');
+// } else if(num === 0) { 
+//     console.log('zero');
+// } else {
+//     console.log('Negative')
+// }
+
+
+// 3️⃣ Double Number
+
+// একটা number নিয়ে সেটাকে double করো।
+
+// let num = 10;
+// num = num * 2
+// console.log(num)
+
+
+
+// 4️⃣ Largest Number
+
+// 3টা number দেওয়া থাকবে।
+// সবচেয়ে বড় number বের করো।
+
+// Example:
+
+// 10, 25, 15
+// Output: 25
+
+// let a = 10;
+// let b = 25;
+// let c = 15;
+
+// if(a > b && a > c) {
+//     console.log('largest Number: ', a)
+// } else if(b > a && b > c) { 
+//     console.log('largest Number: ', b);
+// } else{ 
+//     console.log('largest Number : ', c)
+// }
+
+
+// 5️⃣ Sum of Two Numbers
+
+// 2টা number যোগ করো।
+
+// let a = 10;
+// let b = 20;
+// let c = a + b;
+// console.log('sum: ',c); 
+
+
+// 6️⃣ Age Check
+
+// যদি age ≥ 18 হয়
+// print করবে "Adult"
+
+// না হলে
+// "Minor"
+
+// let age = 18;
+
+// if(age >= 18) {
+//     console.log('Adult');
+// } else{
+//     console.log('Minor');
+// }
+
+
+// 7️⃣ Multiplication
+
+// একটা number এর 5 এর নামতা print করো।
+
+// Example:
+
+// 5 x 1 = 5
+// 5 x 2 = 10
+ 
+ 
+ for(let i = 1; i <= 10; i++) {
+
+      console.log('5 * ' + i + ' = ', (5 * i));
+ }
